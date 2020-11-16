0f6c9d473d5083d7a784a711aa83e8f76ca84e3a
AlexCampos98 <72565364+AlexCampos98@users.noreply.github.com>
2020-11-16 23:00:28 +0100

    Intento de enlace con la base de datos. A la hora de enlazar con la BD, no entra.

--- app/src/main/java/com/example/nordboxapp/login.java
+++ app/src/main/java/com/example/nordboxapp/login.java
@@ -7,22 +7,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class login extends AppCompatActivity implements View.OnClickListener {
 
     TextView tvLoginError;
     EditText etEmailLogin, etPasswordLogin;
     Button btnLogin;
+    usuario idUsuario;
 
-
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        requestQueue = Volley.newRequestQueue(this);
+
         //Inicializado de componentes
         initUI();
+
+        btnLogin.setOnClickListener(this);
     }
 
     //Metodo creado para inicializar todos los componentes de la activity
@@ -38,15 +54,47 @@ public class login extends AppCompatActivity implements View.OnClickListener {
         int id = v.getId();
         if(id == R.id.btnLogin){
 
+            String email = etEmailLogin.getText().toString().trim();
+            String password = etPasswordLogin.getText().toString().trim();
 
+            if(verificacionLogin(email, password)) {
+                idUsuario.getEmail();
+            }
             //Poner en caso de error. Despues de verificar si el usuario es correcto.
             //tvLoginError.setVisibility(View.VISIBLE);
         }
     }
 
-    private void verificacionLogin(){
-        //JsonObjectRequest
+    private boolean verificacionLogin(String email, String password){
+        final boolean[] verificado = new boolean[1];
+        String URL = "http://192.168.1.254/android/login.php?email=" + email + "&password=" + password;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String email;
+                        try {
+                            email = response.getString("email");
 
+                            idUsuario = new usuario(email);
+                            verificado[0] =true;
+                        } catch (JSONException e) {
+                            e.printStackTrace();
 
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        verificado[0] =false;
+                    }
+                }
+        );
+        requestQueue.add(jsonObjectRequest);
+        return verificado[0];
     }
 }
