da3a106872e836d7ca5eb8ec80e2b3616c9e9820
AlexCampos98 <72565364+AlexCampos98@users.noreply.github.com>
2020-11-20 16:59:01 +0100

    Menu overflow logrado con exito.

--- app/src/main/java/com/example/nordboxapp/menuActivity.java
+++ app/src/main/java/com/example/nordboxapp/menuActivity.java
@@ -3,6 +3,9 @@ package com.example.nordboxapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class menuActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class menuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    //Metodo para mostrar y ocultar el menu
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_overflow, menu);
+        return true;
+    }
+
+    //Metodopara asignar las funciones al menu
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.overReservar){
+            Toast.makeText(this, "Aaaaaaaa", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
