140f9c1052076dfbeea1b3d03ee868d1140308bd
AlexCampos98 <72565364+AlexCampos98@users.noreply.github.com>
2021-02-23 00:26:28 +0100

    Añadida animacion splashScreen

---
+++ app/src/main/java/com/example/nordboxapp/Splash.java
@@ -0,0 +1,55 @@
+package com.example.nordboxapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        //Animaciones
+        Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animation2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        final TextView textView = findViewById(R.id.tvSplash);
+        final ImageView imageView = findViewById(R.id.logoSplash);
+
+        textView.setAnimation(animation2);
+        imageView.setAnimation(animation1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, login.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(imageView, "imgLogin");
+                pairs[1] = new Pair<View, String>(textView, "tvLogin");
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Splash.this, pairs);
+                    startActivity(intent, options.toBundle());
+                } else {
+                    startActivity(intent);
+                }
+                finish();
+            }
+        }, 4000);
+    }
+}
