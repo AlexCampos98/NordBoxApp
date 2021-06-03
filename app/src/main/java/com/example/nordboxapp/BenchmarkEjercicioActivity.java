b9e6abc8459b553e2b626ddca0d196f83e8db643
AlexCampos98 <72565364+AlexCampos98@users.noreply.github.com>
2021-06-03 21:03:37 +0200

    Creada la logica de apuntar ejercicios, ahora cuando le das a guardar los datos, se guardan en la BD y se agregan a la tabla.

--- app/src/main/java/com/example/nordboxapp/BenchmarkEjercicioActivity.java
+++ app/src/main/java/com/example/nordboxapp/BenchmarkEjercicioActivity.java
@@ -2,32 +2,46 @@ package com.example.nordboxapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Iterator;
 
+import nordboxcad.ApuntoEjercicio;
 import nordboxcad.EjercicioBenchUsuario;
 import nordboxcad.EjerciciosBench;
+import nordboxcad.Evento;
 import nordboxcad.NordBoxCADCliente;
 
 public class BenchmarkEjercicioActivity extends AppCompatActivity implements View.OnClickListener{
 
     UsuarioStatico usuarioStatico = new UsuarioStatico();
 
-    Integer idEjercicio;
+    EditText etEjercicioPeso, etEjercicioRonda, etEjercicioFecha;
+    TextInputLayout inpEjercicioPeso, inpEjercicioRonda, inpEjercicioFecha;
     TableLayout tablaEjerciciosBench;
     Button btnEjercicioEnviar;
+    ImageButton btnEjercicioFecha;
+
+    Integer idEjercicio;
     EjercicioBenchUsuario ejerciciosUsuario;
     ArrayList<EjercicioBenchUsuario> ejercicioBenchUsuarios;
+    ApuntoEjercicio apuntoEjercicio;
 
     boolean esperaHilo = true;
 
@@ -44,9 +58,17 @@ public class BenchmarkEjercicioActivity extends AppCompatActivity implements Vie
     }
 
     private void InitUI() {
+        etEjercicioPeso = findViewById(R.id.etEjercicioPeso);
+        etEjercicioRonda = findViewById(R.id.etEjercicioRonda);
+        etEjercicioFecha = findViewById(R.id.etEjercicioFecha);
+        inpEjercicioPeso = findViewById(R.id.inpEjercicioPeso);
+        inpEjercicioRonda = findViewById(R.id.inpEjercicioRonda);
+        inpEjercicioFecha = findViewById(R.id.inpEjercicioFecha);
         tablaEjerciciosBench = findViewById(R.id.tablaEjerciciosBench);
         btnEjercicioEnviar = findViewById(R.id.btnEjercicioEnviar);
         btnEjercicioEnviar.setOnClickListener(this);
+        btnEjercicioFecha = findViewById(R.id.btnEjercicioFecha);
+        btnEjercicioFecha.setOnClickListener(this);
     }
 
     //Metodo usado para agregar filas en la tabla
@@ -111,10 +133,113 @@ public class BenchmarkEjercicioActivity extends AppCompatActivity implements Vie
         }
     }
 
+    public void abrirCalendario(View view){
+        Calendar calendar = Calendar.getInstance();
+        int anio = calendar.get(Calendar.YEAR);
+        int mes = calendar.get(Calendar.MONTH);
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog pickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String fecha = year + "-" + (month+1) + "-" + dayOfMonth;
+                etEjercicioFecha.setText(fecha);
+            }
+        }, anio, mes , dia);
+        pickerDialog.show();
+    }
+
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        //TODO tengo el id, ahora tengo que sacar los datos de la tabla, si es que hay. Y hacer que pueda agregar datos nuevos.
+        if (id == R.id.btnEjercicioFecha){
+            abrirCalendario(v);
+        }
+        else if (id == R.id.btnEjercicioEnviar) {
+            //Comprobaciones campo por campo, de los errores en ellos.  inpEjercicioPeso, inpEjercicioRonda, inpEjercicioFecha
+            boolean comprobacion = true;
+            if (etEjercicioPeso.getText().toString().equals("")) {
+                inpEjercicioPeso.setErrorEnabled(true);
+                inpEjercicioPeso.setError(getString(R.string.errorObligatorio));
+                comprobacion = false;
+            } else{
+                inpEjercicioPeso.setErrorEnabled(false);
+            }
+            if (etEjercicioRonda.getText().toString().equals("")) {
+                inpEjercicioRonda.setErrorEnabled(true);
+                inpEjercicioRonda.setError(getString(R.string.errorObligatorio));
+                comprobacion = false;
+            } else{
+                inpEjercicioRonda.setErrorEnabled(false);
+            }
+            if (etEjercicioFecha.getText().toString().equals("")) {
+                inpEjercicioFecha.setErrorEnabled(true);
+                inpEjercicioFecha.setError(getString(R.string.errorObligatorio));
+                comprobacion = false;
+            } else{
+                inpEjercicioFecha.setErrorEnabled(false);
+            }
 
+            if(comprobacion){
+
+                apuntoEjercicio = new ApuntoEjercicio(idEjercicio, usuarioStatico.getUsuario().getId(), etEjercicioFecha.getText().toString(), Integer.parseInt(etEjercicioPeso.getText().toString()), Integer.parseInt(etEjercicioRonda.getText().toString()));
+                //Inicio un hilo para comprobar que el usuario este en la BD.
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Conectamos con el servidor.
+                        NordBoxCADCliente nordBoxCAD = new NordBoxCADCliente("10.0.2.2", 30501);
+
+
+                        nordBoxCAD.crearEjeBench(apuntoEjercicio);
+
+                        //Cambio el valor del controlador de espera de hilo.
+                        esperaHilo = true;
+                    }
+                });
+                thread.start();
+
+                //Bucle que espera a que el hilo termine.
+                boolean bucleEspFinHilo = true;
+                while (bucleEspFinHilo) {
+                    if (esperaHilo) {
+                        bucleEspFinHilo = false;
+                    }
+                }
+
+                TableRow fila = new TableRow(this);
+
+                //Defino el layout de la fila
+                TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+                fila.setLayoutParams(lp);
+
+                //Estilo al borde
+                fila.setBackgroundResource(R.drawable.borde_tabla);
+
+                TextView textViewRondas = new TextView(this);
+                TextView textViewPeso = new TextView(this);
+                TextView textViewFecha = new TextView(this);
+                textViewRondas.setBackgroundResource(R.drawable.borde_tabla);
+                textViewPeso.setBackgroundResource(R.drawable.borde_tabla);
+                textViewFecha.setBackgroundResource(R.drawable.borde_tabla);
+
+                textViewRondas.setText(" " + apuntoEjercicio.getnRondas());
+                textViewPeso.setText(" " + apuntoEjercicio.getPeso());
+                textViewFecha.setText(" " + apuntoEjercicio.getFecha());
+
+                fila.addView(textViewRondas);
+                fila.addView(textViewPeso);
+                fila.addView(textViewFecha);
+
+                //Agregar fila a la tabla
+                tablaEjerciciosBench.addView(fila);
+
+                etEjercicioFecha.setText(null);
+                etEjercicioPeso.setText(null);
+                etEjercicioRonda.setText(null);
+//                finish();
+//                startActivity(getIntent());
+            }
+        }
     }
 }
