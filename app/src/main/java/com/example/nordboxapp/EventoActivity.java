cade84b2c1d63676531b78fc1fe7288102127b0e
AlexCampos98 <72565364+AlexCampos98@users.noreply.github.com>
2021-06-05 23:29:07 +0200

    Añadida la logica para seleccionar el dia al que se quiere utilizar el calendario.

--- app/src/main/java/com/example/nordboxapp/EventoActivity.java
+++ app/src/main/java/com/example/nordboxapp/EventoActivity.java
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import nordboxcad.EjerciciosBench;
 import nordboxcad.Evento;
 import nordboxcad.NordBoxCADCliente;
 
-public class EventoActivity extends AppCompatActivity {
+public class EventoActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageButton btnDiaAnterior, btnDiaSiguiente, btnEventoCalendario;
+    TextView tvEventoFecha;
+    static Calendar calendar;
+    static String fechaActual;
 
     //Para el recycler
     List<Evento> elements;
@@ -25,11 +39,27 @@ public class EventoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evento);
 
-        //Borrar
+        if(calendar == null){
+            calendar = Calendar.getInstance();
+            fechaActual = calendar.get(Calendar.YEAR) + "-" + (calendar.get(Calendar.MONTH)+1) + "-" + calendar.get(Calendar.DAY_OF_MONTH);
+        }
+
         Evento evento = new Evento();
-        evento.setFecha("2021/06/05");
+        evento.setFecha(fechaActual);
 
         recycler(evento);
+        InitUI();
+        tvEventoFecha.setText(fechaActual);
+    }
+
+    private void InitUI() {
+        btnDiaAnterior = findViewById(R.id.btnDiaAnterior);
+        btnDiaAnterior.setOnClickListener(this);
+        btnDiaSiguiente = findViewById(R.id.btnDiaSiguiente);
+        btnDiaSiguiente.setOnClickListener(this);
+        btnEventoCalendario = findViewById(R.id.btnEventoCalendario);
+        btnEventoCalendario.setOnClickListener(this);
+        tvEventoFecha = findViewById(R.id.tvEventoFecha);
     }
 
     /**
@@ -68,5 +98,51 @@ public class EventoActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(listAdapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.btnDiaAnterior)
+        {
+            calendar.set(calendar.get(Calendar.YEAR), (calendar.get(Calendar.MONTH)), calendar.get(Calendar.DAY_OF_MONTH)-1);
+            fechaActual = calendar.get(Calendar.YEAR) + "-" + (calendar.get(Calendar.MONTH)) + "-" + calendar.get(Calendar.DAY_OF_MONTH);
+            reiniciarActivity(this);
+        } else if (id == R.id.btnDiaSiguiente) {
+            calendar.set(calendar.get(Calendar.YEAR), (calendar.get(Calendar.MONTH)), calendar.get(Calendar.DAY_OF_MONTH)+1);
+            fechaActual = calendar.get(Calendar.YEAR) + "-" + (calendar.get(Calendar.MONTH)) + "-" + calendar.get(Calendar.DAY_OF_MONTH);
+            reiniciarActivity(this);
+        } else if (id == R.id.btnEventoCalendario){
+            abrirCalendario(v);
+        }
+
+    }
+
+    //reinicia una Activity
+    public static void reiniciarActivity(Activity actividad){
+        Intent intent=new Intent();
+        intent.setClass(actividad, actividad.getClass());
+        //llamamos a la actividad
+        actividad.startActivity(intent);
+        //finalizamos la actividad actual
+        actividad.finish();
+    }
+
+    public void abrirCalendario(View view){
+        final Calendar calendar = Calendar.getInstance();
+        int anio = calendar.get(Calendar.YEAR);
+        int mes = calendar.get(Calendar.MONTH);
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog pickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(year,month,dayOfMonth);
+                fechaActual = calendar.get(Calendar.YEAR) + "-" + (calendar.get(Calendar.MONTH)) + "-" + calendar.get(Calendar.DAY_OF_MONTH);
+                finish();
+                startActivity(getIntent());
+            }
+        }, anio, mes , dia);
+        pickerDialog.show();
     }
 }
